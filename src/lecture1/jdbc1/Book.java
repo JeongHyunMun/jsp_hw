2f86cffcc0f8a586eec0189e17ce8305fd1c518b
gusan <gusan@DESKTOP-51TVSL5>
2020-05-06 12:20:52 +0900

    시험 20분 경과입니다.

---
+++ src/lecture1/jdbc1/Book.java
@@ -0,0 +1,60 @@
+package lecture1.jdbc1;
+
+public class Book {
+
+	int id;
+	String title;
+	String name;
+	String category;
+	int price;
+	String publisher;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+}
---
+++ src/lecture1/jdbc1/BookDAO.java
@@ -0,0 +1,34 @@
+package lecture1.jdbc1;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import lecture1.DB;
+
+public class BookDAO {
+
+    public static List<Book> findAll() throws Exception {
+        String sql = "SELECT b.*, c.categoryName " +
+                     "FROM book b LEFT JOIN category c ON b.categoryId = c.id";
+        try (Connection connection = DB.getConnection("book");
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+            ArrayList<Book> list = new ArrayList<Book>();
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setId(resultSet.getInt("id"));
+                book.setTitle(resultSet.getString("title"));
+                book.setName(resultSet.getString("name"));
+                book.setCategory(resultSet.getString("category"));
+                book.setPrice(resultSet.getInt("price"));
+                book.setPublisher(resultSet.getString("publisher"));
+                list.add(book);
+            }
+            return list;
+        }
+    }
+}
+
